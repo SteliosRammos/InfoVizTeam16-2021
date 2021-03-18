195bab16596e83ed99a41c0d5fa0753b98fde152
Stelios Rammos <stelio.rammos@gmail.com>
2021-03-18 13:29:42 +0100

    modify sql for creation year range

--- app/selection.js
+++ app/selection.js
@@ -3,7 +3,7 @@ const db = DBConfig.db;
 
 // Fetches the artworks matching the given parameters
 // Input: dictionnary with a class for each categorical variable: 
-// century, nationality, artowrk type, general type and school
+// century, nationality, artwork type, general type and school
 // Output: the dominant colors, the artworks and their distance from the average 
 // dominant color
 function construct_sql_query(parameters) {
@@ -12,14 +12,11 @@ function construct_sql_query(parameters) {
     
     let where_clause = Object.entries(parameters).reduce((where_clause, [key, value]) => {
 
-        value = (key == 'century') ? value : "'" + value + "'"
-        
-        if (first) {
-            where_clause += "WHERE " + key + " = " + value;
-            first = false;
-        } else {
-            where_clause += " AND " + key + " = " + value
-        }
+        if (value.length == 0) { return where_clause};
+
+        append = (key == 'creation_year') ? key + " BETWEEN " + value['begin'] + " AND " + value['end']: key + " = '" + value + "'";
+
+        where_clause += where_clause.length == 0 ? "WHERE " + append : " AND " + append;
 
         return where_clause
     }, '')
